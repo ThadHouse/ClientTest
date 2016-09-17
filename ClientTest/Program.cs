dd96f126212809d0a52db0326956d8d9e52a3603
Thad House <thadhouse1@gmail.com>
2016-09-17 10:15:21 -0700

    Testing

--- ClientTest/Program.cs
+++ ClientTest/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using NetworkTables;
 
 namespace ClientTest
 {
@@ -10,6 +12,22 @@ namespace ClientTest
     {
         static void Main(string[] args)
         {
+            NetworkTable.SetClientMode();
+            NetworkTable.SetIPAddress("localhost");
+
+            NetworkTable.Initialize();
+
+            Thread.Sleep(1000);
+
+            NetworkTable tbl = NetworkTable.GetTable("Test");
+            Console.WriteLine(tbl.IsConnected);
+
+            foreach (var connectionInfo in NtCore.GetConnections())
+            {
+                Console.WriteLine(connectionInfo);
+            }
+
+            Thread.Sleep(Timeout.Infinite);
         }
     }
 }
